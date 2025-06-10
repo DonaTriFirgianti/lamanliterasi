555143293e2ef0f0cc6f1813e91ef3fe854282c4
Dona Tri Firgianti <148875227+DonaTriFirgianti@users.noreply.github.com>
2025-06-10 17:58:45 +0700

    Delete inventaris_barang directory

--- inventaris_barang/resources/views/components/application-logo.blade.php
+++
@@ -1,3 +0,0 @@
-<svg viewBox="0 0 316 316" xmlns="http://www.w3.org/2000/svg" {{ $attributes }}>
-    <path fill="currentColor" d="M4 8h16v13a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V8Zm3-3V3a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v2h4v4H2V5h4Zm2-1v1h6V4H9Z"/>
-</svg>
